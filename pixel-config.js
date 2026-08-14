2dfeb265b4b4d0ff50cf11bfa0eae171fe753b19
Floskinou <meyer.florent@gmail.com>
2026-08-14 06:02:16 -0300

    feat: add remote Meta Pixel activation switch

---
+++ pixel-config.js
@@ -0,0 +1,8 @@
+/* Remote switch for GTM-WGL79Z Meta Pixel.
+ * Change "true" to "false" to disable the pixel remotely.
+ */
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "facebook_pixel_config_ready",
+  facebook_pixel_enabled: "true"
+});
