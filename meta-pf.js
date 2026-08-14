bbc4f8b5a89b8ab9f9bd00eebb9c639a12e47eeb
Floskinou <meyer.florent@gmail.com>
2026-08-14 06:47:33 -0300

    chore: rename remote switch to neutral nomenclature

---
+++ meta-pf.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "pf_ready",
+  pf_enabled: "false"
+});
--- pixel-config.js
+++
@@ -1,8 +0,0 @@
-/* Remote switch for GTM-WGL79Z Meta Pixel.
- * Change "true" to "false" to disable the pixel remotely.
- */
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-  event: "facebook_pixel_config_ready",
-  facebook_pixel_enabled: "false"
-});
