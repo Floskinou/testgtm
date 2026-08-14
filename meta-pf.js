6e07f28e8194415d65088f027762ffc2de8d7ed4
Floskinou <meyer.florent@gmail.com>
2026-08-14 08:15:57 -0300

    test: enable GA4 flag

--- meta-pf.js
+++ meta-pf.js
@@ -2,7 +2,7 @@ window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
   event: "pf_ready",
   pf_enabled: "false",
-  ga4_en: "false",
+  ga4_en: "true",
   li_en: "false",
   ads_en: "false",
   ttk_en: "false"
