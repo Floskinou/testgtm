ae1222be034dfe6d727e00d086da63317e195f49
Florent Meyer <florent@escale-ads.com>
2026-09-07 10:44:51 +0200

    chore: restore all Epikur flags on after preview tests

--- meta-pf-GTM-N9ZNNX8.js
+++ meta-pf-GTM-N9ZNNX8.js
@@ -2,9 +2,9 @@ window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
  event: "pf_ready",
  gtm_container: "GTM-N9ZNNX8",
- pf_enabled: "false",
- ga4_en: "false",
- li_en: "false",
- ads_en: "false",
- ttk_en: "false"
+ pf_enabled: "true",
+ ga4_en: "true",
+ li_en: "true",
+ ads_en: "true",
+ ttk_en: "true"
 });
